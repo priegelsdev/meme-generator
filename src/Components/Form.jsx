b177df6df00ecc26e83b09798779274c7077ac26
priegelsdev <priegelsdev@gmail.com>
2023-01-19 09:53:36 +0100

    add placeholder properties

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -2,8 +2,8 @@ export default function Form() {
   return (
     <main>
       <form>
-        <input className="top-caption" type="text"></input>
-        <input className="bottom-caption" type="text"></input>
+        <input className="top-caption" type="text" placeholder="Top caption"></input>
+        <input className="bottom-caption" type="text" placeholder="Bottom caption"></input>
         <button className="submit-btn">Get meme</button>
       </form>
     </main>
